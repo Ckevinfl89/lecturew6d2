89557e210bdd67de25df12125b69691c72e85334
christian sanabria <ckevin89@hotmail.com>
2023-08-15 16:14:47 -0500

    added data entry

---
+++ app/routes.py
@@ -0,0 +1,31 @@
+from app import app, db
+from flask import render_template, redirect, url_for
+from app.forms import SignUpForm
+from app.models import User
+
+# Add a route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        # Get the data from the form
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        username = form.username.data
+        email = form.email.data
+        password = form.password.data
+        print(first_name, last_name, username, email, password)
+
+        # Create a new instance of the User class with data from form
+        new_user = User(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+        # Add the new_user object to the database
+        db.session.add(new_user)
+        db.session.commit()
+        # redirect back to the home page
+        return redirect(url_for('index'))
+    
+    return render_template('signup.html', form=form)
