80b76186250659b0cbf0636adee80f2956a1d28b
christian sanabria <ckevin89@hotmail.com>
2023-08-16 11:55:37 -0500

    updates

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from random import randint
 
 # DDL Statement
 # CREATE TABLE user(
@@ -26,3 +26,14 @@ class User(db.Model):
     
     def check_password(self, password_guess):
         return check_password_hash(self.password, password_guess)
+
+def random_photo():
+    return f"https://picsum.photos/500?random={randint(1,100)}"
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    body = db.Column(db.String, nullable=False)
+    image_url = db.Column(db.String, nullable=False, )
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
